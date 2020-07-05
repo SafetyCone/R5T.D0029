c3338f527480da566143cfac07d4003d863609a5
MinexAutomation <minex.automation@gmail.com>
2020-07-05 11:19:19 -0700

    Added R5T.D0029.Base library. Added IVisualStudioProjectFileSerializer and IAsFilePathVisualStudioProjectFileSerializer service definition interface types.

---
+++ source/R5T.D0029.Base/Code/Services/Definitions/IVisualStudioProjectFileSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.D0013;
+using R5T.T0002;
+
+
+namespace R5T.D0029
+{
+    public interface IVisualStudioProjectFileSerializer : IFileSerializer<IVisualStudioProjectFile>
+    {
+    }
+}

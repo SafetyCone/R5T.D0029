964d9073ef9dabec7dd2a6736caa169cacbe969a
MinexAutomation <minex.automation@gmail.com>
2020-07-10 23:05:47 -0700

    Added R5T.D0029.Standard project. Added IServiceCollection extensions.

---
+++ source/R5T.D0029.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.D0001;
+using R5T.D0010;
+using R5T.D0017.Default;
+using R5T.D0019.Default;
+using R5T.D0021.Default;
+using R5T.D0022.Default;
+using R5T.D0029.Default;
+
+using R5T.Dacia;
+using R5T.Lombardy;
+
+
+namespace R5T.D0029.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddVisualStudioProjectFileSerializer(this IServiceCollection services,
+            IServiceAction<INowUtcProvider> nowUtcProviderAction,
+            IServiceAction<IMessageSink> messageSinkAction,
+            IServiceAction<IStringlyTypedPathOperator> stringlyTypedPathOperatorAction)
+        {
+            // 0.
+            var visualStudioProjectFileTransformerAction = services.AddVisualStudioProjectFileTransformerAction();
+            var visualStudioProjectFileXDocumentPrettifierAction = services.AddVisualStudioProjectFileXDocumentPrettifierAction();
+
+            // 1.
+            var functionalVisualStudioProjectFileSerializationModifierAction = services.AddFunctionalVisualStudioProjectFileSerializationModifierAction(
+                stringlyTypedPathOperatorAction);
+            var relativePathsXDocumentVisualStudioProjectFileStreamSerializerAction = services.AddRelativePathsXDocumentVisualStudioProjectFileStreamSerializerAction(
+                nowUtcProviderAction);
+
+            // 2.
+            var xDocumentVisualStudioProjectFileSerializerAction = services.AddXDocumentVisualStudioProjectFileSerializerAction(
+                relativePathsXDocumentVisualStudioProjectFileStreamSerializerAction,
+                functionalVisualStudioProjectFileSerializationModifierAction,
+                messageSinkAction,
+                visualStudioProjectFileXDocumentPrettifierAction);
+
+            services.AddVisualStudioProjectFileSerializer(
+                visualStudioProjectFileTransformerAction,
+                xDocumentVisualStudioProjectFileSerializerAction);
+
+            return services;
+        }
+    }
+}
